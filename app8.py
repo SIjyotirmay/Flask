835a1ed16735995d3df4fbe7775ec595f7c60404
Jyotirmay Shrestha <jyotirmayshrestha7@gmail.com>
2025-05-05 02:59:26 +0530

    file uploaded

--- app8.py
+++ app8.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/') 
 def main(): 
-	return render_template("index.html") 
+	return render_template("upload.html") 
 
 @app.route('/success', methods = ['POST']) 
 def success(): 
 	if request.method == 'POST': 
 		f = request.files['file'] 
 		f.save(f.filename) 
-		return render_template("Acknowledgement.html", name = f.filename) 
+		return render_template("ack.html", name = f.filename) 
 
 if __name__ == '__main__': 
 	app.run(debug=True)
